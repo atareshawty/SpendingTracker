e465f74618cfc1beb9b030f0dae8f455521ae0ca
Alex Tareshawty <atareshawty@gmail.com>
2015-07-30 10:12:33 -0400

    Some organizational things

--- charting.js
+++
@@ -1,6 +0,0 @@
-require(['js/Chartjs']), function(Chart) {
-  var Chartjs = Chart.noConflict();
-};
-
-var context = document.getElementById("mainChart").getContext("2d");
-var chart = new Chart(context).PolarArea(42);

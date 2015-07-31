b270549b1106eb885288f10edd7313beb780e0dd
Alex Tareshawty <atareshawty@gmail.com>
2015-07-31 10:35:37 -0400

    Restructured files changed require js out with jquery. Removed what I had done earlier for clarity

--- js/charting.js
+++
@@ -1,6 +0,0 @@
-requirejs(["Chartjs/Chartjs"]), function(Chart) {
-  var Chartjs = Chart.noConflict();
-};
-
-var context = document.getElementById("mainChart").getContext("2d");
-var chart = new Chartjs(context).PolarArea(42);

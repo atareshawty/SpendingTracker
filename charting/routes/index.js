b308912fee7fb535993889a7058197c4085e7d15
Alex Tareshawty <atareshawty@gmail.com>
2015-08-22 17:24:46 -0400

    Added postgres. No more hard coded data soon

--- charting/routes/index.js
+++ charting/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/user_spending';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {

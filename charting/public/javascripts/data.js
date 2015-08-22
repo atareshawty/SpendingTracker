f913ecdad9aca175ff7b7db5c3d5c950275769b9
Alex Tareshawty <atareshawty@gmail.com>
2015-08-22 12:51:36 -0400

    Can create chart in canvas from separate js file and jquery

--- charting/public/javascripts/data.js
+++ charting/public/javascripts/data.js
@@ -1,7 +1,26 @@
 window.onload = function() {
-  var canvas = document.getElementById("mainChart");
+  var canvas = $("#mainChart").get(0);
   var context = canvas.getContext("2d");
-  context.rect(0,0,200,100);
-  context.fillStyle = "green";
-  context.fill();
+  var data = [
+    {
+        value: 300,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 100,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    }
+  ];
+
+  var chart = new Chart(context).Pie(data);
 }

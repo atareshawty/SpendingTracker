3fa1fa16523d21b7cec870355928414d378ce9c0
Alex Tareshawty <atareshawty@gmail.com>
2015-08-22 16:18:08 -0400

    Added new route for about page

--- charting/routes/about.js
+++ charting/routes/about.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('users', { title: 'Sample User'});
+  res.send('I like building things!');
 });
 
 module.exports = router;

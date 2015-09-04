27bad80230739721f5179351d7438fa470526388
Alex Tareshawty <atareshawty@gmail.com>
2015-09-03 22:34:45 -0400

    Removed models directory. Not needed

--- charting/models/database.js
+++
@@ -1,7 +0,0 @@
-var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/user_spending';
-
-var client = new pg.Client(connectionString);
-client.connect();
-var query = client.query('INSERT INTO spending VALUES (1, \'Russell\', 100.25)');
-query.on('end', function() { client.end(); });

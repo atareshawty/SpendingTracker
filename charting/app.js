eba4c029e0199c2d9ec09008a6c0cc3b30040fcb
Alex Tareshawty <atareshawty@gmail.com>
2015-09-06 14:51:26 -0400

    User can only access users/# if their id matches the url id and removed some debugging

--- charting/app.js
+++ charting/app.js
@@ -33,7 +33,6 @@ passport.use(new Strategy(
     db.users.findByUsername(username, function(err, user) {
       if (err) { return cb(err); }
       if (!user) { return cb(null, false); }
-      console.log('Passwords match: ' + bcrypt.compareSync(password, user.password));
       if (!bcrypt.compareSync(password, user.password)) { return cb(null, false); }
       return cb(null, user);
     });
--- charting/routes/users.js
+++ charting/routes/users.js
@@ -1,14 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('users', { title: 'Sample User'});
 });
 
 router.get('/:id', function(req, res, next) {
-  console.log(req.user.spending);
-  res.render('user', { user: req.user });
+  //User id request matches logged in user id
+  if (req.session.passport && req.session.passport.user) {
+    console.log('url: ' + req.url + '  User id: ' + req.session.passport.user.id);
+    if (userMatchesURLReq(req.url, req.session.passport.user.id)) {
+      res.render('user', { user: req.user });
+    } else {
+      res.send('Don\'t try to access another user\'s information!');
+    }
+
+  } else {
+
+    res.redirect('/login');
+
+  }
 });
+
+var userMatchesURLReq = function(reqURL, userId) {
+  return reqURL == ('/' + userId);
+}
 
 module.exports = router;

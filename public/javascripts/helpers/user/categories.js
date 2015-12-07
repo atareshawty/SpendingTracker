6cfeebabcab6c7a43feb096b17b07696dc112399
Alex Tareshawty <atareshawty@gmail.com>
2015-12-06 20:48:19 -0500

    Moved all jquery for user.handlebars to a sub directory of js helpers and separated different forms into separate helper files. Changed setup.js to make username in table login to varchar(20) to match local

---
+++ public/javascripts/helpers/user/categories.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('#customCategory').on('keyup', function() {
+		var category = $('#customCategory').val();
+		if (0 < category.length && category.length <= 20) {
+			$("input[id=submitCategory]").attr("disabled", false);
+		} else {
+			$("input[id=submitCategory]").attr("disabled", true);
+		}		
+	});
+});

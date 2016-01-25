dc01d0e3257dfb70ed48ba8e9ba2ef3d08f5472d
Alex Tareshawty <atareshawty@gmail.com>
2016-01-25 16:42:25 -0500

    Changed package.json and db/setup.js to fix initial installation

--- db/setup.js
+++ db/setup.js
@@ -18,7 +18,7 @@ client.query('CREATE TABLE users (id serial primary key, username varchar(30) UN
 client.query('CREATE TABLE spending (id int NOT NULL DEFAULT 0.00, cost float NOT NULL, category varchar(20), location varchar(30), date varchar(20))');
 client.query('CREATE TABLE categories (id int, category varchar(20), UNIQUE(id, category))');
 client.query('ALTER TABLE users ADD CONSTRAINT username_length CHECK (char_length(password) > 0)');
-client.query('ALTER TABLE spending ADD CONSTRAINT purchase_length CHECK(char_length(category) > 0 AND char_length(location) > 0 AND char_length(date > 0))');
+client.query('ALTER TABLE spending ADD CONSTRAINT purchase_length CHECK(char_length(category) > 0 AND char_length(location) > 0 AND char_length(date) > 0)');
 var finalQuery = client.query('ALTER TABLE categories ADD CONSTRAINT category_length CHECK(char_length(category) > 0)');
 
 finalQuery.on('end', function() {

b1eca1324e99f42c6a8f81809b47ea6bd0ff95fa
Alex Tareshawty <atareshawty@gmail.com>
2016-02-18 17:06:49 -0500

    Changed category form handling into module and fixed some linting stuff

--- public/javascripts/helpers/user/category.js
+++ public/javascripts/helpers/user/category.js
@@ -1,58 +1,72 @@
-function validateCategories() {
-  $('.category-form #customCategory').on('keyup', validateNewCategory);
-  $('.category-form #submitCategory').on('click', addNewCategory);
-}
-
-function validateNewCategory() {
-  var category = $('.category-form #customCategory').val();
-  if (0 < category.length && category.length <= 20) {
-    $('.category-form #submitCategory').attr("disabled", false);
-    return true;
-  } else {
-    $('.category-form #submitCategory').attr("disabled", true);
-    return false;
-  }  
-}
-
-function addNewCategory() {
-  if (validateNewCategory() && !checkForDuplicateCategory()) {
-    var category = $('.category-form #customCategory').val();
-    sendNewCategoryWithFetch(category);
-    App.addUserCategory(category);
-    $('#POST-category').append($('<option>', {value: category, text: category}));
-  } else if (!validateNewCategory()) {
-    alert('The length of your new category should be <= 20');
-  } else if (checkForDuplicateCategory()) {
-    alert('That category already exists');
-  }
-  clearNewCategory();
-  $('html, body').animate({ scrollTop: 0 }, 'medium');
-}
-
-function checkForDuplicateCategory() {
-  var category = $('.category-form #customCategory').val();
-  var hasDuplicate = false;
-  $("select[name='category'] > option").each(function () {
-    if (category.toLowerCase() === this.text.toLowerCase()) {
-      hasDuplicate = true;
-      //break out of loop
-      return false;
+/* global fetch */
+/* global App */
+/* global $ */
+var Categories = (function() {
+  var categoryInput;
+  var categorySubmitButton;
+  var categoryDropDown;
+   
+  return {
+    startCategoryFormListener: function() {
+      categoryInput = $('.category-form #customCategory');
+      categorySubmitButton = $('.category-form #submitCategory');
+      categoryDropDown = $('#POST-category');
+      categoryInput.on('keyup', validateNewCategory);
+      categorySubmitButton.on('click', addNewCategory);
     }
-  });
-  return hasDuplicate; 
-}
+  };
+  
+  function validateNewCategory() {
+    var category = categoryInput.val();
+    if (0 < category.length && category.length <= 20) {
+      categorySubmitButton.attr("disabled", false);
+      return true;
+    } else {
+      categorySubmitButton.attr("disabled", true);
+      return false;
+    }  
+  }
 
-function clearNewCategory() {
-  $('.category-form #submitCategory').attr("disabled", true);
-  $('.category-form #customCategory').val('')
-}
+  function addNewCategory() {
+    if (validateNewCategory() && !checkForDuplicateCategory()) {
+      var category = categoryInput.val();
+      sendNewCategoryWithFetch(category);
+      App.addUserCategory(category);
+      categoryDropDown.append($('<option>', {value: category, text: category}));
+    } else if (!validateNewCategory()) {
+      alert('The length of your new category should be <= 20');
+    } else if (checkForDuplicateCategory()) {
+      alert('That category already exists');
+    }
+    clearNewCategory();
+    $('html, body').animate({ scrollTop: 0 }, 'medium');
+  }
 
-function sendNewCategoryWithFetch(category) {
-  var href = document.URL;
-  var username = href.substring(href.lastIndexOf('/') + 1);
-  var url = '/api/category/' + username + '/?category=' + category;
-  fetch(url, {
-    credentials: 'same-origin',
-    method: 'post'
-  });
-}
+  function checkForDuplicateCategory() {
+    var category = categoryInput.val();
+    var hasDuplicate = false;
+    $("select[name='category'] > option").each(function () {
+      if (category.toLowerCase() === this.text.toLowerCase()) {
+        hasDuplicate = true;
+        //break out of loop
+        return false;
+      }
+    });
+    return hasDuplicate; 
+  }
+
+  function clearNewCategory() {
+    categorySubmitButton.attr("disabled", true);
+    categoryInput.val('')
+  }
+
+  function sendNewCategoryWithFetch(category) {
+    var href = document.URL;
+    var username = href.substring(href.lastIndexOf('/') + 1);
+    var url = '/api/category/' + username + '/?category=' + category;
+    fetch(url, {
+      credentials: 'same-origin',
+      method: 'post'
+    });
+  }
+}());
--- public/javascripts/userFetch.js
+++ public/javascripts/userFetch.js
@@ -1,3 +1,4 @@
+/* global Handlebars */
 var href = document.URL;
 var username = href.substring(href.lastIndexOf('/') + 1);
 var result = fetch('/api/users/' + username, {
@@ -14,7 +15,7 @@ result.then(function(response) {
   App.buildTable();
   App.buildIncomeTable();
   App.buildCompareChart();
-  validateCategories();
+  Categories.startCategoryFormListener();
   validateAndCreateDateRange();
   validateAndCreatePurchaseInput();
 }).catch(function(err) {

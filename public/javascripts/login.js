b60f0af10da0346c544d0233c03300ceb92d4102
Alex Tareshawty <atareshawty@gmail.com>
2016-02-22 20:56:41 -0500

    Updated eslint config. Fixed some client side files

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
   var password = $('#inputForm input[name=password]');
   $('#inputForm input[name=username], #inputForm input[name=password]').on('keyup', function() {
     if (username.val().length > 0 && password.val().length > 0) {
-      $("input[type=submit]").attr("disabled", false);
+      $('input[type=submit]').attr('disabled', false);
     } else {
-      $("input[type=submit]").attr("disabled", true);
+      $('input[type=submit]').attr('disabled', true);
     }      
   });
 });
